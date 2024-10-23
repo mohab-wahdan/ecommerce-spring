d3514d01b54b27cb39869cb769ea8bc5ee738f91
waleedelhamey <waleedkamal1999@gmail.com>
2024-10-23 16:50:40 +0300

    Update Order and OrderItems

--- src/main/java/com/example/ecommerce/controllers/OrderController.java
+++ src/main/java/com/example/ecommerce/controllers/OrderController.java
@@ -5,12 +5,10 @@ import com.example.ecommerce.services.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/orders")
 public class OrderController {
     OrderService orderService;
 
